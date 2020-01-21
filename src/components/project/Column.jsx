78e807885c8f289ff2a5a6b8baf36fcd6f7f50f7
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-01-20 18:53:15 -0800

    created scroll function

--- src/components/project/Column.jsx
+++ src/components/project/Column.jsx
@@ -9,12 +9,17 @@ export default class Column extends Component {
     return (
       <>
         <h2>{this.props.column.title}</h2>
+
         <Droppable
           droppableId={this.props.column.id}
           isDropDisabled={this.props.isDropDisabled}
         >
           {(provided, snapshot) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}>
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="column_container"
+            >
               {this.props.tasks &&
                 this.props.tasks.map((task, index) => (
                   <Task key={task.id} task={task} index={index} />
